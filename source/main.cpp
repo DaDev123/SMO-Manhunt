264b6c70fc379b00b55a548aa8e4bafbf415b6f3
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-06-11 21:32:42 +0200

    Removing Boss Barrier in Bowser's (main.cpp)

--- source/main.cpp
+++ source/main.cpp
@@ -503,6 +503,29 @@ void seadPrintHook(const char* fmt, ...) {
     va_end(args);
 }
 
+// ManHunt bool and stuff
+
+namespace al {
+    bool trySyncStageSwitchAppearAndKill(LiveActor*);
+    const char* getModelName(const LiveActor* actor);
+    void startNerveAction(LiveActor*, const char*);
+}
+
+void barrierAppearHook(al::LiveActor* thisPtr, const char* actionName) {
+    if (!GameModeManager::instance()->isModeAndActive(GameMode::HIDEANDSEEK)) {
+        return; // Disable functionality if not in HIDEANDSEEK mode
+    }
+
+    if (al::isEqualString(GameDataFunction::getCurrentStageName(thisPtr), "SkyWorldHomeStage") && 
+        al::calcDistanceH(thisPtr, sead::Vector3f{5722.f, 29000.f, -41583.f}) < 200) {
+        // thisPtr->kill();
+        al::startNerveAction(thisPtr, "Disappear");
+    } else {
+        al::startNerveAction(thisPtr, actionName);
+    }
+}
+
+
 bool compassAlwaysVisible(GameDataHolderAccessor accessor) {
     return GameModeManager::instance()->isModeAndActive(GameMode::HIDEANDSEEK);
 }
