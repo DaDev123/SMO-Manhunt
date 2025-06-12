2448a72a42f1b8ce8b51f2eeb312be3150829a09
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-06-12 09:34:25 +0200

    Fixed Compiling issues (?)

--- source/hooksManHunt.cpp
+++ source/hooksManHunt.cpp
@@ -1,3 +1,42 @@
+#include <cmath>
+#include <math.h>
+
+#include "al/execute/ExecuteOrder.h"
+#include "al/execute/ExecuteTable.h"
+#include "al/execute/ExecuteTableHolderDraw.h"
+#include "al/LiveActor/LiveActor.h"
+#include "al/util.hpp"
+#include "al/util/AudioUtil.h"
+#include "al/util/CameraUtil.h"
+#include "al/util/ControllerUtil.h"
+#include "al/util/GraphicsUtil.h"
+#include "al/util/LiveActorUtil.h"
+#include "al/util/NerveUtil.h"
+#include "al/util/DemoUtil.h"
+
+#include "game/GameData/GameDataHolderAccessor.h"
+#include "game/GameData/GameDataFunction.h"
+#include "game/HakoniwaSequence/HakoniwaSequence.h"
+#include "game/Player/PlayerActorBase.h"
+#include "game/Player/PlayerActorHakoniwa.h"
+#include "game/Player/PlayerFunction.h"
+#include "game/Player/PlayerHackKeeper.h"
+#include "game/StageScene/StageScene.h"
+
+#include "logger.hpp"
+
+#include "sead/container/seadSafeArray.h"
+#include "sead/gfx/seadPrimitiveRenderer.h"
+#include "sead/heap/seadHeap.h"
+#include "sead/math/seadVector.h"
+
+#include "server/gamemode/GameModeBase.hpp"
+#include "server/gamemode/GameModeFactory.hpp"
+#include "server/gamemode/GameModeManager.hpp"
+
+#include "rs/util.hpp"
+
+
 bool manhuntSequenceHook(HakoniwaSequence* sequence) {
     StageScene* stageScene = (StageScene*)sequence->curScene;
 
